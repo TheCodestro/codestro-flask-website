b4cb938f4699538c6bcc285d6b3fedd279578252
Brian <kailash@calliope-u>
2015-03-31 18:49:01 -0600

    Fixed to be old flaskbook base

--- premier.py
+++ premier.py
@@ -10,7 +10,7 @@ app.wsgi_app = ProxyFix(app.wsgi_app)
 
 @app.route('/favicon.ico')
 def favicon():
-	return send_from_directory(os.path.join(app.root_path, 'static'),
+	return send_from_directory(os.path.join(app.root_path, 'static'), 
 				   'favicon.ico', mimetype='image/vnd.microsoft.icon')
 @app.route('/')
 def index():
